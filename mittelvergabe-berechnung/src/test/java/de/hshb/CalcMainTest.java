21f484a11e40b7f7f30f6e1ff4fc0a40e5378ff3
dragondagda <steve@steveliedtke.de>
2016-05-15 20:53:20 +0200

    wrote simple unit test for CalcMain.java

---
+++ mittelvergabe-berechnung/src/test/java/de/hshb/CalcMainTest.java
@@ -0,0 +1,38 @@
+package de.hshb;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import info.solidsoft.mockito.java8.api.WithBDDMockito;
+import io.vertx.core.Vertx;
+
+public class CalcMainTest implements WithBDDMockito{
+
+	private CalcMain calcMain;
+	
+	@Mock
+	private Vertx vertxMock;
+
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		
+		calcMain = new CalcMain();
+		
+		Whitebox.setInternalState(calcMain, "vertx", vertxMock);
+	}
+	
+	@Test
+	public void testStart() throws Exception  {
+	
+		// @WHEN
+		calcMain.start();
+	
+		// @THEN
+		then(vertxMock).should().deployVerticle(CalcVerticle.class.getName());
+	}
+
+}
